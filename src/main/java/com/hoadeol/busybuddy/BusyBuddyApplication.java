e4674f76e558b2f81e28cd98b33d265d52365373
mi-hwa <43jmih@naver.com>
2024-01-24 12:38:02 +0900

    프로젝트 생성

---
+++ src/main/java/com/hoadeol/busybuddy/BusyBuddyApplication.java
@@ -0,0 +1,13 @@
+package com.hoadeol.busybuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusyBuddyApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BusyBuddyApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/hoadeol/busybuddy/BusyBuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hoadeol.busybuddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusyBuddyApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
