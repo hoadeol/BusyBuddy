ae1dd57eb84fbbb054956d4faa9fb220cca9274b
mi-hwa <43jmih@naver.com>
2024-01-30 11:20:34 +0900

    validation 속성 추가

--- src/main/java/com/hoadeol/busybuddy/model/Member.java
+++ src/main/java/com/hoadeol/busybuddy/model/Member.java
@@ -5,6 +5,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import java.time.LocalDateTime;
@@ -24,18 +25,25 @@ public class Member {
   @Column(name = "MEMBER_ID", length = 30)
   @Size(max = 30)
   private String id;
+
   @Column(nullable = false, length = 30)
   @Size(max = 30)
   private String memberName;
+
   @Column(unique = true, length = 50)
   @Email
   private String email;
+
   @Column(nullable = false, length = 100)
   @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*[!@#$%^&*()-_=+]).{8,}$",
       message = "비밀번호는 8문자 이상이어야 하며, 특수문자 1개와 영문자 1개가 포함되어야 합니다.")
+  @Size(max = 100)
+  @NotBlank
   private String password;
+
   @Column(name = "REG_DT")
   private LocalDateTime registrationDate = LocalDateTime.now();
+
   @Column(name = "LAST_LOGIN_DT")
   private LocalDateTime lastLoginDate = LocalDateTime.now();
 
--- src/main/java/com/hoadeol/busybuddy/model/Task.java
+++ src/main/java/com/hoadeol/busybuddy/model/Task.java
@@ -9,6 +9,8 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import java.time.LocalDateTime;
 import lombok.AccessLevel;
@@ -26,26 +28,37 @@ public class Task {
   @Id
   @Column(name = "TASK_ID")
   private Long id;
+
   @ManyToOne(fetch = FetchType.LAZY)
   @JoinColumn(name = "MEMBER_ID", nullable = false)
   private Member member;
+
   @ManyToOne(fetch = FetchType.LAZY)
   @JoinColumn(name = "CATE_ID")
   private Category category;
+
   @Column(length = 50)
-  @Size(max = 50)
+  @Size(min = 1, max = 50)
+  @NotBlank
   private String title;
+
   @Column(length = 500)
   @Size(max = 500)
   private String content;
+
+  @Future(message = "마감일은 현재 시간 이후여야 합니다.")
   @Column(name = "DUE_DT")
   private LocalDateTime dueDate = LocalDateTime.now().toLocalDate().plusDays(1).atStartOfDay();
+
   @Enumerated(EnumType.STRING)
   private Priority priority = Priority.NORMAL;
+
   @Column(name = "COMPLETE_YN")
   private Boolean isCompleted = false;
+
   @Column(name = "COMPLETE_DT")
   private LocalDateTime completeDate;
+
   @Column(name = "REG_DT")
   private LocalDateTime registrationDate = LocalDateTime.now();
 }
