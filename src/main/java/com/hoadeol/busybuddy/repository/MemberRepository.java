58a1f6815857bbdb75e9328d8cabc2293c23fd65
mi-hwa <43jmih@naver.com>
2024-01-30 14:15:24 +0900

    Repository 추가

---
+++ src/main/java/com/hoadeol/busybuddy/repository/MemberRepository.java
@@ -0,0 +1,8 @@
+package com.hoadeol.busybuddy.repository;
+
+import com.hoadeol.busybuddy.model.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<Member, String> {
+
+}
