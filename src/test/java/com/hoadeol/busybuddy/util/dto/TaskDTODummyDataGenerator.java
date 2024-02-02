45140a01cafffdce4b1a17fe289eab6326981fb5
mi-hwa <43jmih@naver.com>
2024-02-02 18:31:01 +0900

    TaskDTODummyDataGenerator 생성

---
+++ src/test/java/com/hoadeol/busybuddy/util/dto/TaskDTODummyDataGenerator.java
@@ -0,0 +1,41 @@
+package com.hoadeol.busybuddy.util.dto;
+
+import com.hoadeol.busybuddy.dto.TaskDTO;
+import com.hoadeol.busybuddy.util.TestDummyDataGenerator;
+import java.time.LocalDate;
+import java.util.Optional;
+
+public class TaskDTODummyDataGenerator extends TestDummyDataGenerator {
+
+  public TaskDTO createSavedTaskDTO(Long memberId, Long categoryId) {
+    //pathVariable value
+    Long id = getRandomId(1, NUM_TASKS);
+
+    //not null value
+    memberId = Optional.ofNullable(memberId).orElse(getRandomId(1, NUM_MEMBERS));
+
+    //nullable values
+    categoryId = Optional.ofNullable(categoryId)
+        .orElse(RANDOM.nextBoolean() ? getRandomId(1, NUM_CATEGORIES) : null);
+    String content = RANDOM.nextBoolean() ? generateRandomString(STRING_LENGTH * 5) : null;
+    String priority = RANDOM.nextBoolean() ? String.valueOf(generateRandomPriority()) : null;
+    LocalDate startDate = RANDOM.nextBoolean() ? LocalDate.from(generateRandomFuture(DAYS)) : null;
+    LocalDate dueDate = RANDOM.nextBoolean() ? LocalDate.from(generateRandomFuture(DAYS)) : null;
+
+    return TaskDTO.builder()
+        .id(id)
+        .memberId(memberId)
+        .categoryId(categoryId)
+        .title(generateRandomString(STRING_LENGTH))
+        .content(content)
+        .priority(priority)
+        .startDate(startDate)
+        .dueDate(dueDate)
+        .build();
+  }
+
+  public TaskDTO createSavedTaskDTO() {
+    return createSavedTaskDTO(null, null);
+  }
+
+}
